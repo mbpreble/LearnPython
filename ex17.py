4ecebb2b1e65a360fc75694cbcce69d6b6fa5f10
Matthew Preble <mbpreble@gmail.com>
2014-04-06 11:11:00 -0500

    Completes Ex 17.

---
+++ ex17.py
@@ -0,0 +1,11 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print "Copying from %s to %s" % (from_file, to_file)
+
+indata = open(from_file).read();
+open(to_file, 'w').write(indata)
+
+print "Alright, all done"
